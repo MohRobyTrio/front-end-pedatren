968d3f98c5385a1b4ca8a2cd0d83f083b66a16f2
allstarbrow <candraaldianto05@gmail.com>
2025-03-05 12:18:44 +0700

    Membuat halaman progres/catatan

--- src/content_main_page/content_tab_formulir/Progress.jsx
+++ src/content_main_page/content_tab_formulir/Progress.jsx
@@ -1,7 +1,35 @@
+import React, { useState } from "react";
+
 const Progress = () => {
+    const [activeTab, setActiveTab] = useState("Afektif");
+
     return (
-        <h1 className="text-xl font-bold">Progress Report Peserta Didik</h1>
+        <>
+        <h1 className="text-xl font-bold">Catatan</h1>
+        <hr className="border-t border-gray-300 my-4" />
+
+        <div className="flex flex-col space-y-2">
+        <button
+          className={`p-2 text-blue-600 rounded ${
+            activeTab === "Afektif" ? "bg-gray-200" : ""
+          }`}
+          onClick={() => setActiveTab("Afektif")}
+        >
+          Afektif
+        </button>
+        <button
+          className={`p-2 text-blue-600 rounded ${
+            activeTab === "Kognitif" ? "bg-gray-200" : ""
+          }`}
+          onClick={() => setActiveTab("Kognitif")}
+        >
+          Kognitif
+        </button>
+      </div>
+        </>
+
     )
 };
 
 export default Progress;
+
