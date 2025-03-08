dba30cf072146428485f0f0cd3f171c9a1420f48
MohRobyTrio <mohrobytriod@gmail.com>
2025-03-08 16:12:47 +0700

    Mengubah component filter agar lebih responsive

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,7 +2,7 @@ const Filters = ({ showFilters, filterOptions }) => {
     if (!showFilters) return null;
     
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full mb-4">
+        <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 w-full mb-4">
             {Object.entries(filterOptions).map(([label, options], index) => (
                 <select key={`${label}-${index}`} className="border border-gray-300 rounded p-2">
                     {options.map((option, idx) => (
