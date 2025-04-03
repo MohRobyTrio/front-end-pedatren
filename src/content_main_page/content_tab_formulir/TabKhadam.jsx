f0ee1bb7dacd853e0ad212528df72e5814168f1b
allstarbrow <candraaldianto05@gmail.com>
2025-04-03 13:12:49 +0700

    membuat tampilan karyawan, pengurus, khadam pada tab formulir

--- src/content_main_page/content_tab_formulir/TabKhadam.jsx
+++ src/content_main_page/content_tab_formulir/TabKhadam.jsx
@@ -1,6 +1,11 @@
 const TabKhadam = () => {
     return (
+        <>
         <h1 className="text-xl font-bold">Khadam</h1>
+        <hr className="border-t border-gray-300 my-4" />
+        <h2 className="text-lg font-semibold mt-1">Haddam KH. Hafiluddin Faqih</h2>
+        <p className="text-gray-600 text-sm mt-1">Sejak 20 Feb 2022 Sampai Sekarang</p>
+        </>
     )
 };
 
--- src/content_main_page/content_tab_formulir/TabPengurus.jsx
+++ src/content_main_page/content_tab_formulir/TabPengurus.jsx
@@ -1,7 +1,42 @@
-const TabPengurus = () => {
-    return (
-        <h1 className="text-xl font-bold">Pengurus</h1>
-    )
-};
+// const TabPengurus = () => {
+//     return (
+//         <h1 className="text-xl font-bold">Pengurus</h1>
+//     )
+// };
 
-export default TabPengurus;
+// export default TabPengurus;
+
+const TabPengurus = () => {
+    const data = [
+      {
+        title: "Kepala Bagian PEPHA, Ortala dan Kepegawaian",
+        period: "Sejak 7 Jan 2018 Sampai 26 Jul 2021"
+      },
+      {
+        title: "Kepala Inkubasi Bisnis Pesantren",
+        period: "Sejak 26 Jun 2021 Sampai 31 Des 2022"
+      },
+      {
+        title: "Sekretaris Pesantren",
+        details: "Tetap | Grade B2",
+        period: "Sejak 1 Jan 2023 Sampai Sekarang"
+      }
+    ];
+  
+    return (
+      <>
+        <h1 className="text-xl font-bold">Pengurus</h1>
+        <hr className="border-t border-gray-300 my-4" />
+        {data.map((item, index) => (
+          <div key={index} className="mt-4">
+            <h2 className="text-lg font-semibold text-gray-900">{item.title}</h2>
+            {item.details && <p className="text-gray-600 text-sm">{item.details}</p>}
+            <p className="text-gray-600 text-sm">{item.period}</p>
+          </div>
+        ))}
+      </>
+    );
+  };
+  
+  export default TabPengurus;
+  
