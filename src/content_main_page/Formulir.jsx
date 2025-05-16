be46c0be2bc95063664e822dee8eaf24382782fc
Muhammad zainul hasan <mzhasan1144@gmail.com>
2025-05-16 10:52:08 +0700

    fix detail kunjungan mahrom

--- src/content_main_page/Formulir.jsx
+++ src/content_main_page/Formulir.jsx
@@ -66,7 +66,7 @@ const Formulir = () => {
   return (
     <div className="flex-1 pl-6 pt-6 pb-6">
       <div className="flex justify-between items-center mb-6">
-        <h1 className="text-2xl font-bold">Formulir ID: {biodata_id}</h1>
+        <h1 className="text-2xl font-bold">Formulir</h1>
       </div>
 
       <div className="bg-white p-6 rounded-lg shadow-md">
