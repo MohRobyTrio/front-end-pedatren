3092b020185e137e00d1cf7609775cda00d2bbcc
Andrydoo <andryyudiono@gmail.com>
2025-08-15 22:39:12 +0700

    update config

--- src/hooks/config.js
+++ src/hooks/config.js
@@ -1,31 +1,36 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const rolesWithAccess = {
+  ///Aksi///
   tambah: ['admin', 'superadmin'],
-  tambahakun: ['admin', 'superadmin'],
   edit: ['admin', 'superadmin'],
   delete: ['admin', 'superadmin'],
-  kelembagaan: ['admin', 'superadmin'],
-  kewilayahan: ['admin', 'superadmin'],
-  akademik: ['admin', 'superadmin'],
   pindah: ['admin', 'superadmin'],
   keluar: ['admin', 'superadmin'],
   approve: ['biktren', 'kamtib', 'pengasuh'],
-  golongan: ["admin", "superadmin"],
+  ///Menu///
+  kelembagaan: ['admin', 'superadmin'],
+  kewilayahan: ['admin', 'superadmin'],
   kewaliasuhan: ["admin", "superadmin"],
-  manage: ["admin", "superadmin"],
   kepegawaian: ["admin", "superadmin"],
-  santri: ["admin", "superadmin"],
-  kitab: ["admin", "superadmin"],
-  karturfid: ["admin", "superadmin"],
+  akademik: ['admin', 'superadmin'],
+  golongan: ["admin", "superadmin"],
+  ///SubMenu///
   dashboard: ["admin", "superadmin"],
-  perizinan: ["admin", "superadmin"],
+  santri: ["admin", "superadmin"],
+  perizinan: ["admin", "superadmin", "biktren", "kamtib", "pengasuh"],
   pelanggaran: ["admin", "superadmin"],
   kunjungan: ["admin", "superadmin"],
+  khadam: ["admin", "superadmin"],
+  ///Fitur///
+  karturfid: ["admin", "superadmin"],
+  kitab: ["admin", "superadmin"],
   sholat: ["admin", "superadmin"],
   jadwal_sholat: ["admin", "superadmin"],
-  khadam: ["admin", "superadmin"],
   presensi_sholat: ["admin", "superadmin", "ustadz"],
   tahfidz: ["admin", "superadmin", "ustadz"],
-  nadhoman: ["admin", "superadmin", "ustadz"]
+  nadhoman: ["admin", "superadmin", "ustadz"],
+  ///Manage///
+  manage: ["admin", "superadmin"],
+  tambahakun: ['admin', 'superadmin']
 };
